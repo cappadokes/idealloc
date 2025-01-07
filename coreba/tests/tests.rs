f68004a003b34e4e1d4b187a525f7a30a721cb9e
Christos P. Lamprakos <cplamprakos@protonmail.com>
2025-01-07 15:32:13 +0200

    Tidied test data

--- coreba/tests/tests.rs
+++ coreba/tests/tests.rs
@@ -6,12 +6,12 @@ fn get_crate_root() -> Result<PathBuf, std::env::VarError> {
     Ok(PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?))
 }
 
-fn read_from_path<T, B>(p: &str) -> Result<JobSet, Box<dyn std::error::Error>> 
+fn read_from_path<T, B>(p: &str, shift: ByteSteps) -> Result<JobSet, Box<dyn std::error::Error>> 
 where T: JobGen<B> {
     let mut file_path = get_crate_root()?;
     file_path.push(p);
     let parser = T::new(file_path);
-    let jobs = parser.read_jobs()?;
+    let jobs = parser.read_jobs(shift)?;
     assert!(jobs.len() > 0);
     let set = coreba::jobset::init(jobs)?;
 
@@ -23,7 +23,7 @@ const MAX_LIVES: u32 = 3;
 
 #[test]
 fn run_hello_plc() {
-    let set = read_from_path::<PLCParser, &[u8; 8 * PLC_FIELDS_NUM]>("tests/data/ldb0.plc").unwrap();
+    let set = read_from_path::<PLCParser, &[u8; 8 * PLC_FIELDS_NUM]>("tests/data/ldb0.plc", 0).unwrap();
     coreba::algo::idealloc(set, MAX_FRAG, 0, MAX_LIVES);
 }
 /*
